52bc45131abc086ab0f30c95cecef2ee1b475a7a
parthparikh <parthparikh1999p@gmail.com>
2019-11-13 03:14:48 +0530

    Added metadata (story, summary, tagline, actors, wins_nominations, release_date) of bollywood movies from 2010 to 2019

--- README.txt
+++ README.txt
@@ -1,9 +1,38 @@
+TIMDB - The Indian Movie Database
+
+Attributes present:
+    './bollywood.csv': title, imdb_id, poster_path,wiki_link
+    './bollywood_meta.csv': imdb_id, title, original_title, is_adult, year_of_release, runtime, genres
+    './bollywood_ratings.csv': imdb_id, imdb_rating, imdb_votes
+    './bollywood_text.csv': imdb_id, story, summary, tagline, actors, wins_nominations, release_date
+
+    UNIQUE (18 attributes):
+        title(wiki), imdb_id, poster_path, wiki_link, original_title,
+        title(imdb), is_adult, year_of_release, runtime, genres,
+        imdb_rating, imdb_votes, story, summary, tagline,
+        actors, wins_nominations, release_date
+
 Note: 
 In './bollywood_ratings.csv' if:
-	value is NaN -> it means the film is yet to be released
-	value is 0 -> No rating was given to the film
+    value is NaN -> it means the film is yet to be released
+    value is 0 -> No rating was given to the film
 
 In './bollywood_meta.csv' if:
-	A title is missing, chances are the title had no info in the imdb dump
-	Or the title is yet to be released
-	(indicated by \N)
+    A title is missing, chances are the title had no info in the imdb dump
+    Or the title is yet to be released
+    (indicated by \N)
+
+    As of this commit, all the is_adult values are 0.
+
+In './bollywood_text.csv':
+    Some inconsistencies are removed, as of this commit some inconsistencies are left to find!
+
+    To separate actors '|' is used
+    
+    'text delimiter' has to be 'None' to view the dataset in LibreOffice Calc
+        as the attributes like story and summary contain "" and '' in them
+
+Future Scope:
+    Plans on expanding this dataset from 2010 to 2019
+                                      to 1950 to 2019
+    Director and Writer information left to be added
---
+++ wiki_bot/wiki_bot/spiders/imdb_scrape.py
@@ -0,0 +1,62 @@
+import scrapy
+import json
+from bs4 import BeautifulSoup
+
+class ImdbScrape(scrapy.Spider):
+	name = "imdbbot"
+	def __init__(self):
+		pass
+
+	def start_requests(self):
+		urls = json.load(open(".././wiki_dump/imdb_ids_2010-2019.json"))
+		for url in urls:
+			yield scrapy.Request(url=('https://www.imdb.com/title/' + url), callback=self.parse)
+
+	def parse(self, response):
+		bs = BeautifulSoup(response.text, 'html5lib')
+		imdb_url = response.request.url
+		imdb_id = imdb_url.replace("https://www.imdb.com/title/", "").replace("/", "")
+		wins_nominations = str()
+		story = str()
+		summary = str()
+		actors = str()
+		tagline = str()
+
+		try:
+			wins_nominations = bs.findAll("span", class_="awards-blurb")[0].text.replace(".", "").strip()
+		except:
+			pass
+
+		try:
+			release_date = bs.findAll("div", class_="subtext")[0].text.strip().split("|")[3].strip()
+		except:
+			try:
+				release_date = bs.findAll("div", class_="subtext")[0].text.strip().split("|")[2].strip()
+			except:
+				release_date = str()
+
+		try:
+			summary = bs.findAll("div", class_="summary_text")[0].text.strip()
+		except:
+			pass
+
+		try:
+			for i in range(1, len(bs.findAll("table", class_="cast_list")[0].findAll("tr"))-1):
+				actors = actors + bs.findAll("table", class_="cast_list")[0].findAll("tr")[i].findAll("td")[1].text.strip() + "|"
+		except:
+			pass
+
+		try:
+			story = bs.findAll("div", id="titleStoryLine")[0].findAll("div", class_="inline canwrap")[0].findAll("span")[0].text.strip()
+		except:
+			pass
+
+		try:
+			if bs.findAll("div", class_="txt-block")[0].findAll("h4")[0].text == "Taglines:":
+				tagline = bs.findAll("div", class_="txt-block")[0].text.replace("Taglines:", "").strip()
+		except:
+			pass
+
+		with open("../bollywood_text.csv", 'a') as f:
+			f.write(imdb_id + "," + story.replace(",", " ").replace("\n", "") + "," + summary.replace(",", " ").replace("See full summary »", "").replace("See more »", "").replace("\n", "") + "," + tagline.replace(",","|").replace("\n", "") + "," + actors.replace("\n", "") + "," + wins_nominations.replace("\n", "") + "," + release_date.replace("\n", "") + "\n")
+		self.log("saved imdb data for %s" % imdb_url)
